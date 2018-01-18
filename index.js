9004386970d6bf11e54b6d6dff29936f6138f417
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-18 23:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var height = "74"
 
 name = "Susan"
 height = 70
+
+console.log("Name:")
+console.log(name)
+
+console.log("Height:")
+console.log(height)
+
 // Don't worry about this
 module.exports = { name, height
 }
